cefca2cb359bc3b5c96682f5c6e099f8be33b6ec
Mira Khan <115938924+m1ra-k@users.noreply.github.com>
2025-02-26 11:59:25 -0500

    Update PipeBehavior.cs

--- Assets/Scripts/Environment/PipeBehavior.cs
+++ Assets/Scripts/Environment/PipeBehavior.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 // TODO
@@ -7,19 +5,13 @@ public class PipeBehavior : MonoBehaviour
 {
     [SerializeField] private float _velocity = 0.5f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
     // Update is called once per frame
     void Update()
     {
         // move the pipe left after frame according to the velocity, make sure to multiply by Time.deltaTime
     }
 
-    private void OTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         // if trigger touches GameObject with Player tag, increase score
     }
